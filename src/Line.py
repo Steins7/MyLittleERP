5c1f133aa56eafa8048b2571864cb0bee2703eea
TheoLemoineENIB <49072229+TheoLemoineENIB@users.noreply.github.com>
2019-05-06 16:06:37 +0200

    Delete Line.py

--- src/Line.py
+++
@@ -1,9 +0,0 @@
-import Date.py
-class Line  :
-	  def __init__(self, name="",iD ="", jour =[0,0],mois=0,annee=0):
-    
-        #public attributes
-        self.date = Date(jour,mois,annee)
-        self.name = name
-        self.iD= iD
-
