bcde2ab52db7746b9817a12a0e4754cf821b665e
Steins <jce.lepage@gmail.com>
2019-05-27 17:17:41 +0200

    Fixed scrollbars

--- src/GUIMLEV1.2.1/DataTable.py
+++ src/GUIMLEV1.2.1/DataTable.py
@@ -11,35 +11,35 @@ class DataTable(tk.Canvas):
         self.xScrollBar.grid(row=3,rowspan=1,column=1,sticky="ew")
 
         self.config(yscrollcommand=self.yScrollBar.set,xscrollcommand=self.xScrollBar.set)
-        self.interior =  tk.Frame(self)
+        self.interior = tk.Frame(self)
         self.create_window(0, 0, window=self.interior, anchor="nw")
         self.grid(row=1,rowspan=2,column=1,sticky="nsew")
-        self.interior.bind('<Configure>',self.resizeCanvas)
-        #self.bind('<Configure>',self.resizeFrame)
+        self.bind('<Configure>',self.resizeCanvas)
         self.table(100,10)
+        self.addtag_all("all")
+        self.height = self.winfo_height()
+        self.width = self.winfo_width()
 
-    #callbacks
+
+
+    #----callbacks
 
     def resizeCanvas(self,event):
-        #size = [self.interior.winfo_reqwidth(), self.interior.winfo_reqheight()]
-        #self.config(scrollregion="0 0 "+str(size[0])+" "+str(size[1]))
-        wscale = float(event.width)/self.winfo_reqwidth()
-        hscale = float(event.height)/self.winfo_reqheight()
+        size = [self.interior.winfo_reqwidth(), self.interior.winfo_reqheight()]
+        self.config(scrollregion="0 0 "+str(size[0])+" "+str(size[1]))
+
+
+    def resizeFrame(self,event):
+        wscale = float(event.width)/self.interior.winfo_width()
+        hscale = float(event.height)/self.interior.winfo_height()
         width = event.width
         height = event.height
         if(height >= 500):
             height = 500
         # resize the canvas 
-        self.config(width=width, height=height)
+        self.interior.config(width=width, height=height)
         # rescale all the objects tagged with the "all" tag
-        self.scale("all",0,0,wscale,hscale)
-
-    def resizeFrame(self,event):
-        width = self.winfo_width()
-        height = self.winfo_height()
-        if(height >= 500):
-            height = 500
-        self.interior.config(width=self.winfo_width(),height=self.winfo_height())
+        self.interior.scale("all",0,0,wscale,hscale)
 
 
     def table(self,nbRow,nbCol):
